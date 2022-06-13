c5ef8a6a9f38277d5712f6ecdb06c2f496814002
Connor Corcoran <connorcorc@gmail.com>
2022-06-13 11:08:16 -0600

    Add logic to check against player/computer choice

--- game.js
+++ game.js
@@ -18,24 +18,32 @@ class Game {
   }
 
   runGame() {
-    if (this.player.choice === 'rock' && this.computer.choice === 'scissors' || this.computer.choice = 'lizard') {
+    if (this.player.choice === 'rock' && (this.computer.choice === 'scissors' || this.computer.choice === 'lizard')) {
       this.playerWins = true;
       output.innerHTML = 'Player wins!'
-      this.player.winCount()
+      this.player.winCount();
       // this.computer.winCount()
-    } else if (this.player.choice === 'paper' && this.computer.choice === 'rock' || this.computer.choice = 'alien') {
+    } else if (this.player.choice === 'paper' && (this.computer.choice === 'rock' || this.computer.choice === 'alien')) {
       this.playerWins = true;
       output.innerHTML = 'Player wins!'
-      this.player.winCount()
-    } else if (this.player.choice === 'scissors' && this.computer.choice === 'paper' || this.computer.choice = 'lizard') {
+      this.player.winCount();
+    } else if (this.player.choice === 'scissors' && (this.computer.choice === 'paper' || this.computer.choice === 'lizard')) {
       this.playerWins = true;
       output.innerHTML = 'Player wins!'
-      this.player.winCount()
-    } else if (this.player.choice === 'lizard' && this.computer.choice === 'paper' || this.computer.choice === 'alien') {
+      this.player.winCount();
+    } else if (this.player.choice === 'lizard' && (this.computer.choice === 'paper' || this.computer.choice === 'alien')) {
       this.playerWins = true;
       output.innerHTML = 'Player wins!'
-      this.player.winCount()
-    } else if (this.player.choice === 'alien' && this.computer.choice === 'scissors' || this.computer.choice === 'rock') 
+      this.player.winCount();
+    } else if (this.player.choice === 'alien' && (this.computer.choice === 'scissors' || this.computer.choice === 'rock')) {
+      this.playerWins = true;
+      output.innerHTML = 'Player wins!'
+      this.player.winCount();
+    } else {
+      this.computer.playerWins = false;
+      output.innerHTML = 'Computer wins!'
+      this.computer.winCount();
+    }
   }
 
   checkForDraw() {
